47406faaa9a0db7145e0c68467faef931146d78f
ho-un Woo <h-woo@colorchips.co.jp>
2018-02-06 17:51:21 +0900

    Add Birthday Cake Candles

---
+++ Algorithms/Warmup/Birthday Cake Candles/README.md
@@ -0,0 +1,9 @@
+# Birthday Cake Candles
+
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+Colleen is having a birthday! She will have a cake with one candle for each year of her age. When she blows out the candles, she’ll only be able to blow out the tallest ones.
+Find and print the number of candles she can successfully blow out.
+
+콜린이 생일을 보내고있습니다.! 그녀는 나이의 해마다 하나의 양초가 든 케이크를 먹을 것입니다. 그녀가 양초를 불 때, 그녀는 가장 키가 큰 것들만 날려 버릴 수 있습니다.
+성공적으로 날아갈 수있는 양초를 찾아서 그 수를 인쇄하십시오.
---
+++ Algorithms/Warmup/Birthday Cake Candles/index.js
@@ -0,0 +1,40 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function birthdayCakeCandles(n, ar) {
+    // Complete this function
+    const max = Math.max.apply(null, ar);
+
+    return ar.reduce((a, x) => {
+        if (max == x) a = a + 1;
+        return a;
+    }, 0);
+}
+
+function main() {
+    var n = parseInt(readLine());
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = birthdayCakeCandles(n, ar);
+    process.stdout.write("" + result + "\n");
+}
+
